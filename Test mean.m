bdd5a4af2f3506dd1c92634412edc37a3afa2a66
Алексей <.com>
2023-02-16 13:01:26 +0300

    Убрал ненужные файлы. Оставил только функции и тестовые файлы

---
+++ Test mean.m
@@ -0,0 +1,1 @@
+%%Test of getting mean function
--- getting_mean.m
+++ getting_mean.m
@@ -1,3 +1,13 @@
-function [mean] = getting_mean(feloop)
-%Пока пустота...
+function [mean] = getting_mean(Some_feloop)
+    E = Some_feloop.E;
+    P = Some_feloop.P;
+    cla
+    plot(E.p, P.p, '.r')
+    plot(E.n, P.n, '.b')
+    
+    xlim([-18 18])
+    ylim([-40 40])
+%     title(['T = ' num2str(Some_feloop.temp) ' °C'], 'FontSize', 18)   
+%     drawnow
+    mean = 6;
 end
