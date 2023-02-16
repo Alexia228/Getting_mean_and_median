9c0a65f533af16fde5f2bde34a7f7ac78c51d415
Алексей <.com>
2023-02-16 16:40:12 +0300

    Сurrent реализована как структура с двумя полями

--- current_from_charge.m
+++ current_from_charge.m
@@ -1,15 +1,11 @@
 %Создаёт данные для токовых петель(берёт производную)
 function [Current] = current_from_charge(Some_feloop) 
 
-global E
-global P
-global time
-
-
-
  E = Some_feloop.E.p(1:end/2); 
  P = Some_feloop.P.p(1:end/2);
  time = Some_feloop.ref.time.p(1:end/2);
- Current = diff(P)./diff(time);
-plot(E(1:end-1), Current*Some_feloop.period)
+ Current.I = diff(P)./diff(time);
+ Current.I(end+1) = Current.I(end)
+ Current.E = E;
+ 
 end 
